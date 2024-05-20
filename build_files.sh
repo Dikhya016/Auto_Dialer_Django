ef313c3226d40b979bd419bc2e71271b285159c5
Dikhya Sahu <140276371+Dikhya016@users.noreply.github.com>
2024-05-20 21:19:09 +0530

    Update build_files.sh (poking the server)

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
  echo "BUILD START. I will build myself"
+ pip3 install django
+ echo "Django is installed in the above line"
  pip3 install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
